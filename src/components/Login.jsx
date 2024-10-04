255957e6be4faaeafe8447fe85433d8895c10c68
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-10-04 21:46:47 +0530

    redirect to login if not authenticated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,8 @@ import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 const Login = () => {
-  const [emailId, setEmailId] = useState("samantha@gmail.com");
-  const [password, setPassword] = useState("Samantha@123");
+  const [emailId, setEmailId] = useState("tamanna@gmail.com");
+  const [password, setPassword] = useState("Tamanna@123");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,58 +1,58 @@
 import { useSelector } from "react-redux";
 import img from "../photo/twitter.png";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
 
   return (
-    <div>
-      <div className="navbar bg-base-200">
-        <div className="flex-1">
+    <div className="navbar bg-base-200">
+      <div className="flex-1">
+        <Link to="/" className=" flex">
           <img src={img} alt="devx-logo" className=" cursor-pointer size-10" />
           <a className="btn btn-ghost text-xl mx-2">DevX</a>
-        </div>
-        {user && (
-          <div className="flex-none gap-2">
-            <div className="form-control">
-              <p className="flex text-lg">
-                Welcome, &nbsp;
-                <span className=" font-semibold text-lg">{user.firstName}</span>
-              </p>
-            </div>
-            <div className="dropdown dropdown-end mx-5 flex">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar"
-              >
-                <div className="w-10 rounded-full">
-                  <img
-                    alt="Tailwind CSS Navbar component"
-                    src={user.photoUrl}
-                  />
-                </div>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-              >
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        )}
+        </Link>
       </div>
+      {user && (
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <p className="flex text-lg">
+              Welcome, &nbsp;
+              <span className=" font-semibold text-lg  cursor-pointer">
+                {user.firstName}
+              </span>
+            </p>
+          </div>
+          <div className="dropdown dropdown-end mx-5 flex">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="user" src={user.photoUrl} />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
